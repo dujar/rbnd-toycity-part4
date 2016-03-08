eaf13640f67611659ce7be6d9b4b826cb9022799
dujar <dujar.coding@gmail.com>
2016-03-08 09:44:09 +0100

    branched out, completed create method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -4,4 +4,22 @@ require 'csv'
 
 class Udacidata
   # Your code goes here!
+  def self.create opts = {}
+    @data_path = File.dirname(__FILE__) + "/../data/data.csv"
+    object = new(id: opts[:id], brand: opts[:brand], name: opts[:name], price: opts[:price])
+####whether object id is a match in data.csv
+    matched = []
+    CSV.foreach(@data_path) do |row|
+        matched << (row[0] == object.id)
+    end
+######
+    if matched.select{|e| e==false}
+      record = [object.id, opts[:brand], opts[:name], opts[:price]]
+      CSV.open(@data_path, "a") do |csv|
+        csv << record 
+      end
+    end
+      
+    object
+  end
 end
