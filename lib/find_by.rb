ecc424f3d64c2a89e036e8864ebaa873111d5f5b
dujar <dujar.coding@gmail.com>
2016-03-09 17:11:09 +0100

    find_by feature done

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -2,6 +2,15 @@ class Module
   def create_finder_methods(*attributes)
     # Your code goes here!
     # Hint: Remember attr_reader and class_eval
-    
+    find_names = ["name","brand"]
+    methods = %Q{
+    find_names.each do |what| 
+    def self.find_by_#{what} product_#{what}
+      all.select{|product| product.#{what} == product_#{what}}.first
+    end
+    end
+    }
+    class_eval(methods)
   end
+
 end
